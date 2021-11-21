c889c895218e4b3683316afbcf4bfdb21bea3361
ghackg11 <gautambehal20@gmail.com>
2021-11-21 13:14:46 +0530

    Profile Page error fixed

--- lib/Screens/profile_page.dart
+++ lib/Screens/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,19 +12,28 @@ class ProfilePage extends StatelessWidget {
     return Material(
       child: Center(
         child: FutureBuilder(
-          future: FirebaseFirestore.instance.collection("UserDetails").doc(FirebaseAuth.instance.currentUser!.uid).get(),
+          future: SharedPreferences.getInstance(),
           builder: (context, snapshot){
-            if(snapshot.connectionState==ConnectionState.done){
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  QrImage(
-                    data: (snapshot.data as DocumentSnapshot)['upiId'],
-                    version: QrVersions.auto,
-                    size: 200.0,
-                  ),
-                  Text('UPI id: ${(snapshot.data as DocumentSnapshot)['upiId']}')
-                ],
+            if(snapshot.hasData){
+              String phoneNumber = (snapshot.data as SharedPreferences).get("phoneNumber")!.toString();
+              return FutureBuilder(
+                future: FirebaseFirestore.instance.collection("User Data").doc(phoneNumber).get(),
+                builder: (context, snapshot){
+                  if(snapshot.connectionState==ConnectionState.done){
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        QrImage(
+                          data: (snapshot.data as DocumentSnapshot)['phoneNumber'],
+                          version: QrVersions.auto,
+                          size: 200.0,
+                        ),
+                        Text('UPI id: ${(snapshot.data as DocumentSnapshot)['phoneNumber']}')
+                      ],
+                    );
+                  }
+                  return Center(child: CircularProgressIndicator());
+                },
               );
             }
             return Center(child: CircularProgressIndicator());
